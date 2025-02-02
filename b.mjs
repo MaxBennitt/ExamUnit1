376881b6bff0aa5012a33d11c574ad98ab086b2a
MaxBennitt <max-leander2@hotmail.com>
2025-02-02 18:57:24 +0100

    Completed part b

--- b.mjs
+++ b.mjs
@@ -20,17 +20,57 @@ import test from "./test.mjs";
 //#region function -----------------------------------------------------------------
 // Write your function her.
 
+function formatName(name) {
+    if (typeof name !== "string") {
+        return null;
+    }
 
+    const trimmedName = name.trim();
+
+    if (trimmedName === "") {
+        return "";
+    }
+
+    if (new RegExp('[!@#$%^&*()_+\\-=\\[\\]{};\':"\\\\|,.<>/?]').test(trimmedName)) {
+        return null;
+    }
+
+    return trimmedName
+        .split(" ")
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+        .join(" ");
+}
 
 //#endregion
-
-
-
-
 
 //#region Tests --------------------------------------------------------------------
 // Write your tests her.
 
+const tests = test("Format Name function");
 
+// Valid inputs
+tests.isEqual(formatName("Tim Tom"), "Tim Tom", "Names with spaces should work");
+tests.isEqual(formatName("timmy"), "Timmy", "Single word should be capitalized");
+tests.isEqual(formatName("james bond"), "James Bond", "Each word should be capitalized");
+tests.isEqual(formatName("JAmEs bOND"), "James Bond", "Uppercase should be properly formatted");
+tests.isEqual(formatName("billy ray cyrus"), "Billy Ray Cyrus", "Multiple words should be capitalized");
+
+// Invalid inputs
+tests.isEqual(formatName(null), null, "Null input should return null");
+tests.isEqual(formatName(undefined), null, "Undefined input should return null");
+tests.isEqual(formatName(123), null, "Number input should return null");
+tests.isEqual(formatName({}), null, "Object input should return null");
+tests.isEqual(formatName([]), null, "Array input should return null");
+tests.isEqual(formatName("#Morris"), null, "Name with # should return null");
+tests.isEqual(formatName("Tar$an"), null, "Name with $ should return null");
+tests.isEqual(formatName("Hannah.Montana"), null, "Name with . should return null");
+tests.isEqual(formatName("Jake!"), null, "Name with ! should return null");
+tests.isEqual(formatName("Dr@ke"), null, "Name with @ should return null");
+
+// Edge cases
+tests.isEqual(formatName("  Jimmy  "), "Jimmy", "Extra spaces should be trimmed");
+tests.isEqual(formatName("  Jimmy Jr  "), "Jimmy Jr", "Multiple spaces should be handled");
+tests.isEqual(formatName(""), "", "Empty string should return empty string");
+tests.isEqual(formatName("   "), "", "String with only spaces should return empty string");
 
 //#endregion
