e98bcdbf47e573ccfeb3efdfac6ae3d93405c797
MaxBennitt <max-leander2@hotmail.com>
2025-02-02 17:14:58 +0100

    Completed part a

--- a.mjs
+++ a.mjs
@@ -20,17 +20,51 @@ import test from "./test.mjs";
 //#region function -----------------------------------------------------------------
 // Write your function her.
 
+function multiply(...numbers) {
+    if (numbers.length === 0) {
+        return 0;
+    }
 
+    const convertedNumbers = numbers.map(num => {
+        if (typeof num === "string") {
+            const converted = Number(num);
+            return converted;
+        }
+        return num;
+    });
+
+    if (convertedNumbers.some(num => typeof num !== "number" || isNaN(num))) {
+        return NaN;
+    }
+
+    return convertedNumbers.reduce((acc, curr) => acc * curr);
+}
 
 //#endregion
-
-
-
-
 
 //#region Tests --------------------------------------------------------------------
 // Write your tests her.
 
+const tests = test("Multiply function");
 
+// Valid inputs
+tests.isEqual(multiply(5, 5), 25, "Multiplying 5 and 5 should be 25");
+tests.isEqual(multiply(3, 4), 12, "Multiplying 3 and 4 should be 12");
+tests.isEqual(multiply("2", 3), 6, 'Multiplying "2" and 3 should be 6');
+tests.isEqual(multiply("2", "3"), 6, 'Multiplying "2" and "3" should be 6');
+
+// Invalid inputs
+tests.isNotANumber(multiply("monkey", 2), 'Multiplying "monkey" and 2 should return NaN');
+tests.isNotANumber(multiply(null, 3), "Multiplying null and 3 should return NaN");
+tests.isNotANumber(multiply(undefined, 4), "Multiplying undefined and 4 should return NaN");
+tests.isNotANumber(multiply(NaN, 5), "Multiplying NaN and 5 should return NaN");
+
+// Edge cases
+tests.isEqual(multiply(), 0, "Multiplying with empty parameter and 0 should return 0");
+tests.isEqual(multiply(7), 7, "Multiplying single number should return that number");
+tests.isEqual(multiply(0, 6), 0, "Multiplying by 0 should return 0");
+tests.isEqual(multiply(Infinity, 2), Infinity, "Multiplying Infinity by positive should be Infinity");
+tests.isEqual(multiply(Infinity, -8), -Infinity, "Multiplying Infinity by negative should be -Infinity");
+tests.isEqual(multiply(-Infinity, -9), Infinity, "Multiplying -Infinity by negative should be Infinity");
 
 //#endregion
